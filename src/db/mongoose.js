156c1980ff3434f4f9a5a362e08fa4bf2ef62fb0
System Administrator <root@MacBook-Pro-de-Marcelino.local>
2021-05-26 16:25:09 +0200

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const mongoURL = process.env.MONGO_URL
+
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true, 
+    useFindAndModify: false
+})
